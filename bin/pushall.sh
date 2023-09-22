672734d404b59f71c87e4e4dc2d8e93eaabbc4b6
phi <phi@freimann.eu>
2023-09-22 16:14:08 +0200

    Skript for git pull push

---
+++ bin/pushall.sh
@@ -0,0 +1,14 @@
+echo "push /cnt/ ..."
+cd cnt
+git push
+echo "... done (pushing /cnt/)"
+
+echo "push /pruefungen/ ..."
+cd ../pruefungen
+git push
+echo "... done (pushing /pruefungen/)"
+
+echo "push /framework/ ..."
+cd ../framework
+git push
+echo "... done (pushing /framework/)"
