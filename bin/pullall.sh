e760895b18529158311520a856f04dc1700a824b
phi <phi@freimann.eu>
2023-10-21 10:39:16 +0200

    Leerzeilen eingefügt zur besseren Lesbarkeit

--- bin/pullall.sh
+++ bin/pullall.sh
@@ -2,13 +2,16 @@ echo "pull /cnt/ ..."
 cd cnt
 git pull
 echo "... done (pulling /cnt/)"
+echo ""
 
 echo "pull /pruefungen/ ..."
 cd ../pruefungen
 git pull
 echo "... done (pulling /pruefungen/)"
+echo ""
 
 echo "pull /framework/ ..."
 cd ../framework
 git pull
 echo "... done (pulling /framework/)"
+echo ""
