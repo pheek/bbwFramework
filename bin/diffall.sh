6e5a24ff0ae6258fc9529011813611ab04a8fa84
phi <phi@freimann.eu>
2023-10-22 10:38:55 +0200

    Leerzeilen zur besseren Leserlichkeit

--- bin/diffall.sh
+++ bin/diffall.sh
@@ -2,13 +2,16 @@ echo "diff /cnt/ ..."
 cd cnt
 git diff
 echo "... done (diff /cnt/)"
+echo
 
 echo "diff /pruefungen/ ..."
 cd ../pruefungen
 git diff
 echo "... done (diff /pruefungen/)"
+echo
 
 echo "diff /framework/ ..."
 cd ../framework
 git diff
 echo "... done (diff /framework/)"
+echo
