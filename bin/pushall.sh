b1088245c7c2c380641d2e8083b359354fb6d3de
phi <phi@freimann.eu>
2023-10-22 10:40:15 +0200

    Leerzeilen

--- bin/pushall.sh
+++ bin/pushall.sh
@@ -2,13 +2,16 @@ echo "push /cnt/ ..."
 cd cnt
 git push
 echo "... done (pushing /cnt/)"
+echo
 
 echo "push /pruefungen/ ..."
 cd ../pruefungen
 git push
 echo "... done (pushing /pruefungen/)"
+echo
 
 echo "push /framework/ ..."
 cd ../framework
 git push
 echo "... done (pushing /framework/)"
+echo
